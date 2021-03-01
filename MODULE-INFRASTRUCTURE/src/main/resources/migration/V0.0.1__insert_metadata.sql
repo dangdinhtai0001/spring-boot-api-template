1d6ae851e2cf9b8852c1277e07dbc1ef5e56faf0
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2021-03-01 19:22:36 +0700

    update metadata

--- MODULE-INFRASTRUCTURE/src/main/resources/migration/V0.0.1__insert_metadata.sql
+++ MODULE-INFRASTRUCTURE/src/main/resources/migration/V0.0.1__insert_metadata.sql
@@ -1,3 +1,7 @@
+-- ---------------------------------------------- user table
+
+INSERT INTO USER (USERNAME, LOCKED, ENABLED)
+VALUES ('root', false, true),  ('admin', false, true);
 -- ---------------------------------------------- role table
 
 INSERT INTO ROLE (NAME)
@@ -13,16 +17,52 @@ VALUES ('GUEST');
 -- permission name = resource_name + __ + privilege_name
 
 INSERT INTO PERMISSION (NAME)
-VALUES ('USER__CREATE');
+VALUES
+('USER__CREATE'),
+('USER__READ'),
+('USER__UPDATE'),
+('USER__DELETE'),
+('ADMIN__CREATE'),
+('ADMIN__READ'),
+('ADMIN__UPDATE'),
+('ADMIN__DELETE')
+;
 
-INSERT INTO PERMISSION (NAME)
-VALUES ('USER__READ');
+-- ---------------------------------------------- role_permission table
 
-INSERT INTO PERMISSION (NAME)
-VALUES ('USER__UPDATE');
+-- permission_id, role_id
+--
+-- select r.name role, group_concat(p.NAME) permission
+-- from role r, permission p, role_permission rp
+-- where r.ID = rp.ROLE_ID and p.ID = rp.PERMISSION_ID;
 
-INSERT INTO PERMISSION (NAME)
-VALUES ('USER__DELETE');
+-- user: root (1)
+-- user: admin (2)
+
+insert into role_permission
+values
+(1,1),
+(2,1),
+(3,1),
+(4,1),
+(5,1),
+(6,1),
+(7,1),
+(8,1),
+(1,2),
+(2,2),
+(3,2),
+(4,2);
+
+-- ---------------------------------------------- user_role table
+
+--user_id, role_id
+
+insert into user_role
+values
+(1,1),
+(2,2)
+;
 
 commit;
 
