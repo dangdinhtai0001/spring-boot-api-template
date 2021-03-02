361f7864c4bfb2f6ebe147f899abebeab5d1ff68
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2021-03-02 10:20:52 +0700

    update init user data script

--- MODULE-INFRASTRUCTURE/src/main/resources/migration/V0.0.1__init_user_data.sql
+++ MODULE-INFRASTRUCTURE/src/main/resources/migration/V0.0.1__init_user_data.sql
@@ -26,13 +26,11 @@ VALUES ('USER__CREATE'),
        ('ADMIN__READ'),
        ('ADMIN__UPDATE'),
        ('ADMIN__DELETE'),
-       ('GRANT__PRIVILEGE'),
-       ('RESET__PASSWORD')
+       ('PRIVILEGE__GRANT'),
+       ('PASSWORD__RESET')
 ;
 
 -- ---------------------------------------------- role_permission table
-
--- permission_id, role_id
 
 insert into role_permission(permission_id, role_id)
 values (1, 1),
@@ -48,14 +46,30 @@ values (1, 1),
        (3, 2),
        (4, 2);
 
-#  ---------------------------------------------- user_role table
-
-# user_id, role_id
+# -------------------------------------------- user_role table
 
 insert into user_role(user_id, role_id)
 values (1, 1),
        (2, 2)
 ;
+
+# -------------------------------------------- create view: VIEW_USER_PERMISSIONS
+
+drop view if exists VIEW_USER_PERMISSIONS;
+
+CREATE VIEW VIEW_USER_PERMISSIONS as
+select
+    u.id user_id, r.name role, p.permissions
+from
+    user u join user_role ur on u.id = ur.USER_ID join role r on r.ID = ur.ROLE_ID,
+    (select r.name role, group_concat(p.name) permissions from
+        role r join role_permission rp on r.id = rp.ROLE_ID
+               join permission p on p.id = rp.PERMISSION_ID
+     group by r.name) p
+where
+        r.name = p.role
+;
+
 
 commit;
 
--- script/database/mysql/test.sql
+++ script/database/mysql/test.sql
@@ -32,7 +32,7 @@ insert into role_permission (ROLE_ID, permission_id) values (1,9), (1,10);
 
 CREATE VIEW VIEW_USER_PERMISSIONS as
 select
-    u.id uer_id, u.USERNAME username, r.name role, p.permissions
+    u.id user_id, r.name role, p.permissions
 from
     user u join user_role ur on u.id = ur.USER_ID join role r on r.ID = ur.ROLE_ID,
     (select r.name role, group_concat(p.name) permissions from
@@ -43,6 +43,19 @@ where
         r.name = p.role
 ;
 
-select * from VIEW_USER_PERMISSIONS;
+drop view if exists VIEW_USER_PERMISSIONS;
 
+select
+    u.id user_id, u.username username , u.email email, u.password password , u.enabled enabled, u.locked locked,
+    u.created_by created_by, u.date_created date_created, u.last_modified_by last_modified_by,
+    u.last_modified_date last_modified_date, r.name role, p.permissions
+from
+    user u join user_role ur on u.id = ur.USER_ID join role r on r.ID = ur.ROLE_ID,
+    (select r.name role, group_concat(p.name) permissions from
+        role r join role_permission rp on r.id = rp.ROLE_ID
+               join permission p on p.id = rp.PERMISSION_ID
+     group by r.name) p
+where
+        r.name = p.role;
 
+select v.user_id, v.role, v.permissions, u.USERNAME from VIEW_USER_PERMISSIONS v join user u on u.id = v.user_id ;
